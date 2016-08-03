1ebd47f5e5443914c3bf79340cf0049eafb03ed0
ikursus <support@usahaniaga.com>
2016-08-03 15:43:32 +0800

    Form

--- resources/views/admin/staff-create.blade.php
+++ resources/views/admin/staff-create.blade.php
@@ -3,26 +3,57 @@
 @section('content')
 <h1>{{ $page_title or "" }}</h1>
 
-<form>
+{!! Form::open() !!}
+
+<div class="form-group">
+  <label for="nama">Nama</label>
+  {!! Form::text('nama', null, [ 'class' => 'form-control'] ) !!}
+</div>
+
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+    <label for="email">Email</label>
+    {!! Form::email('email', null, [ 'class' => 'form-control'] ) !!}
   </div>
+
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="nama">Kad Pengenalan</label>
+    {!! Form::text('kad_pengenalan', null, [ 'class' => 'form-control'] ) !!}
   </div>
+
   <div class="form-group">
-    <label for="exampleInputFile">File input</label>
-    <input type="file" id="exampleInputFile">
-    <p class="help-block">Example block-level help text here.</p>
+    <label for="nama">Tarikh Lahir</label>
+    {!! Form::date('tarikh_lahir', null, [ 'class' => 'form-control'] ) !!}
   </div>
-  <div class="checkbox">
-    <label>
-      <input type="checkbox"> Check me out
-    </label>
+
+  <div class="form-group">
+    <label for="nama">Telefon</label>
+    {!! Form::text('telefon', null, [ 'class' => 'form-control'] ) !!}
   </div>
+
+  <div class="form-group">
+    <label for="nama">Alamat</label>
+    {!! Form::textarea('alamat', null, [ 'class' => 'form-control'] ) !!}
+  </div>
+
+  <div class="form-group">
+    <label for="nama">Password</label>
+    {!! Form::password('password', [ 'class' => 'form-control'] ) !!}
+  </div>
+
+  <div class="form-group">
+    <label for="nama">Nama</label>
+    {!! Form::select('role', array('admin' => 'Admin', 'user' => 'User'),  null, [ 'class' => 'form-control'] ) !!}
+  </div>
+
+  <div class="form-group">
+    <label for="nama">Unit</label>
+    {!! Form::text('unit', null, [ 'class' => 'form-control'] ) !!}
+  </div>
+
+
+
   <button type="submit" class="btn btn-default">Submit</button>
-</form>
+
+{!! Form::close() !!}
 
 @endsection
