c063a29565fc067d1409f7cbe19fcd5a2570c505
ikursus <support@usahaniaga.com>
2016-08-03 12:33:02 +0800

    Update class

--- database/seeds/StaffSeeder.php
+++ database/seeds/StaffSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder {
+class StaffSeeder extends Seeder {
 
 	/**
 	 * Run the database seeds.
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder {
 
 		DB::table('staff')->delete();
 
+		// Staff 1
     Staff::create([
 			'nama' => 'Ali Baba',
 			'email' => 'ali@baba.com',
@@ -23,6 +24,17 @@ class DatabaseSeeder extends Seeder {
 			'alamat' => 'No. 123, Taman Maju Jaya, 70000 Seremban.',
 			'role' => 'admin',
 			'unit' => 'umum'
+		]);
+
+		// Staff 2
+    Staff::create([
+			'nama' => 'Ahmad Albab',
+			'email' => 'ahmad@albab.com',
+			'kad_pengenalan' => '999999-05-5555',
+			'telefon' => '012-8765432',
+			'alamat' => 'No. 123, Taman Sentiasa Maju, 70000 Seremban.',
+			'role' => 'user',
+			'unit' => 'kewangan'
 		]);
 
 	}
